64e37aa1cb6db5b76fcb0af768dca88015191c67
kev494 <kevin.schaefer13@gmail.com>
2018-07-25 17:05:59 +0200

    changed repo

---
+++ PSWCMA/Private/Get-Configurations.ps1
@@ -0,0 +1,43 @@
+﻿Function Get-Configurations {
+    <#
+      .Synopsis
+      Downloads Configurations from your Git Repo
+
+      .Description
+      Downloads Configurations from your Git Repo
+
+      .Outputs
+
+      #>
+
+    [CmdletBinding()]
+    param(
+        <#[Parameter(Mandatory = $true)]
+        $ConfigurationNames,#>
+        [Parameter(Mandatory = $true)]
+        $GitServer,
+        [Parameter(Mandatory = $true)]
+        $Path
+    )
+    begin {
+        $CloneDirectory = "$Path\Configuration"
+        #$Counter = 0
+        $SiteAvail = Invoke-WebRequest $GitServer -DisableKeepAlive -UseBasicParsing -Method head -ErrorAction SilentlyContinue
+    }
+    process {
+        if ($SiteAvail) {
+            if(Test-Path -Path $CloneDirectory) {
+                Start-Process "git" -ArgumentList "-C $CloneDirectory pull" -Wait
+            } else {
+                Start-Process "git" -ArgumentList "clone $GitServer $CloneDirectory" -Wait
+            }
+        } else {
+            Write-Error "Git Repository is not available"
+        }
+
+    }
+    end {
+        #return $Counter
+    }
+
+}
---
+++ PSWCMA/Private/Test-Prerequisites.ps1
@@ -0,0 +1,45 @@
+﻿Function Test-Prerequisites {
+    <#
+      .Synopsis
+      Tests if all prerequisites for the module to work are installed
+
+      .Description
+      Tests if all prerequisites for the module to work are installed
+
+      .Outputs
+      PSObject. Test-prequisites returns a PSObject with three bool values Git, Win10 and All.
+
+
+  #>
+    [CmdletBinding()]
+    param()
+
+    begin {
+        $WindowsVer = [System.Environment]::OSVersion.Version
+        $Git = Get-Command -Name git -ErrorAction SilentlyContinue
+        $WinRM = Test-WSMan -ErrorAction SilentlyContinue
+        $XPSDSC = Get-DscResource -Module 'xPSDesiredStateConfiguration' | Select-Object -First 1
+        $CFW = Get-DscResource -Module 'cFirewall' | Select-Object -First 1
+
+
+    }
+    process {
+        $Prereq = New-Object -TypeName psobject -Property @{
+            All    = $false
+            Git    = $null -ne $Git
+            Win10  = $WindowsVer.Major -eq 10
+            WinRM  = $null -ne $WinRM
+            XPSDSC = $null -ne $XPSDSC
+            CFW    = $null -ne $CFW
+
+        }
+
+        if ($Prereq.Git -and $Prereq.CFW -and $Prereq.Win10 -and $Prereq.WinRM -and $Prereq.XPSDSC) {
+            $Prereq.All = $true
+        }
+    }
+    end {
+        return $Prereq
+    }
+
+}
