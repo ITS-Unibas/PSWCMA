4dc33774b8261c7a807b89e62d232a2ea300b35e
kev494 <kevin.schaefer13@gmail.com>
2018-07-30 17:13:04 +0200

    implemented hashing to check for actual updated files

---
+++ PSWCMA/Private/Update-FileHash.ps1
@@ -0,0 +1,68 @@
+function Update-FileHash {
+    <#
+      .Synopsis
+      Updates the file hashes for all files which could be applied
+
+      .Description
+      Updates the file hashes for all files which could be applied
+
+      .Parameter GroupNames
+      Array of the Groupnames which the hash of the underlaying files should be updated
+
+      .Parameter Path
+      File Path to the json File
+
+#>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [Alias('g')]
+        $GroupNames,
+        [Parameter(Mandatory = $true)]
+        [Alias('p')]
+        [string] $Path
+    )
+
+    begin {
+        $JsonFile = 'FileHashes.json'
+        $Folder = "Configuration"
+    }
+    process {
+        if (Test-Path -Path $Path\$JsonFile) {
+            $Hashes = Get-Content -Path $Path\$JsonFile | ConvertFrom-Json
+        }
+        else {
+            $Hashes = @"
+            {
+                "FileHashes":{
+
+                }
+            }
+"@
+            $Hashes = ConvertFrom-Json $Hashes
+        }
+        foreach ($GroupName in $GroupNames) {
+            $FileName = "$($GroupName.Name).ps1"
+            $FileHash = (Get-FileHash -Path "$Path\$Folder\$($GroupName.Name)\$FileName").Hash
+            $HashObject = @"
+        {
+            "File" : "$FileName",
+            "Hash" : "$FileHash"
+        }
+"@
+            if ($Hashes.FileHashes | Select-Object -Property $FileName) {
+                $Hashes.FileHashes | Select-Object -ExpandProperty $FileName | Where-Object {
+                     $_.Hash = $FileHash }
+            }
+            else {
+                $Hashes.FileHashes | Add-Member -Name $FileName -Value (ConvertFrom-Json -InputObject $HashObject) -MemberType NoteProperty
+            }
+        }
+        if ($Hashes) {
+            $Hashes | ConvertTo-Json | Out-File $Path\$JsonFile -Force
+        }
+    }
+    end {
+
+    }
+}
