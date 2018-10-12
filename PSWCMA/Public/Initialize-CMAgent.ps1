6c978921e3ce5eb8c08a285703741dded61fc002
kev494 <kevin.schaefer13@gmail.com>
2018-10-12 10:39:50 +0200

    added param -NonInteractive to scheduler

--- PSWCMA/Public/Initialize-CMAgent.ps1
+++ PSWCMA/Public/Initialize-CMAgent.ps1
@@ -163,7 +163,7 @@
             #Configure local scheduler with Sechedule.Service COM object
             $TaskProgram = 'powershell'
             $TaskName = 'Configuration Management Agent'
-            $TaskArgs = '-ExecutionPolicy bypass -NoProfile -WindowStyle Hidden -command "& {Import-Module PSWCMA; Install-Configurations}"'            
+            $TaskArgs = '-ExecutionPolicy bypass -NoProfile -NonInteractive -WindowStyle Hidden -command "& {Import-Module PSWCMA; Install-Configurations}"'            
             $Service = New-Object -ComObject("Schedule.Service")
             $Service.Connect()
             $RootFolder = $Service.GetFolder("\")
@@ -174,6 +174,7 @@
             $TaskDefinition.Settings.RunOnlyIfNetworkAvailable = $true
             $TaskDefinition.Settings.DisallowStartIfOnBatteries = $false
             $TaskDefinition.Settings.StopIfGoingOnBatteries = $false
+            $TaskDefinition.Settings.Compatibility = 2
             $TaskDefinition.Principal.RunLevel = 1
             $Triggers = $TaskDefinition.Triggers.Create(1)
             $Triggers.Enabled = $true
